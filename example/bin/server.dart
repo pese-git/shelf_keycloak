64c4fa78178e444db40ab49e12b818afb82253cf
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2023-03-05 14:12:10 +0300

    refactored code

--- example/bin/server.dart
+++ example/bin/server.dart
@@ -1,9 +1,29 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
+import 'package:logger/logger.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_keycloak/shelf_keycloak.dart';
 import 'package:shelf_router/shelf_router.dart';
+
+const clientID = 'own-pub-server-backend';
+const clientSecret = '062WkWIsQz8TwEDaOYk9KwcdM8tT3VqU';
+const configURL =
+    'http://localhost:51510/realms/own-pub-server/.well-known/openid-configuration';
+const redirectURL = 'http://localhost:8080/callback';
+const scopes = ["openid", "profile", "email"];
+
+final authorizationEndpoint = Uri.parse(
+    'http://localhost:51510/realms/own-pub-server/protocol/openid-connect/auth');
+final tokenEndpoint = Uri.parse(
+    'http://localhost:51510/realms/own-pub-server/protocol/openid-connect/token');
+
+final redirectUrl = Uri.parse('http://localhost:8080');
+
+final issuer = 'http://localhost:51510/realms/own-pub-server';
+
+final credentialsFile = File('~/.myapp/credentials.json');
 
 // Configure routes.
 final _router = Router()
@@ -26,7 +46,20 @@ void main(List<String> args) async {
   // Configure a pipeline that logs requests.
   final handler = Pipeline()
       .addMiddleware(logRequests())
-      .addMiddleware(createKeycloakMiddleware())
+      .addMiddleware(
+        createKeycloakMiddleware(
+          logger: Logger(),
+          client: http.Client(),
+          //clientID: clientID,
+          //clientSecret: clientSecret,
+          configURL: Uri.parse(
+              'http://localhost:51510/realms/code-fort-realm/protocol/openid-connect/certs'),
+          kid: "X-cZn2Rk_wZ2Aoh_ESwUk2aalEe_WzQhB-oaQ27isfk",
+          //redirectURL: redirectURL,
+          //issuer: issuer,
+          //scopes: scopes,
+        ),
+      )
       .addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
--- lib/src/shelf_keycloak_base.dart
+++ lib/src/shelf_keycloak_base.dart
@@ -1,14 +1,69 @@
-import 'package:shelf/shelf.dart';
+import 'dart:async';
+import 'dart:convert';
 
-Middleware createKeycloakMiddleware() => (innerHandler) {
-      return (request) {
-        print('Authorization: ${request.headers['Authorization']}');
+import 'package:jose/jose.dart';
+import 'package:logger/logger.dart';
+import 'package:shelf/shelf.dart';
+import 'package:http/http.dart' as http;
+
+Middleware createKeycloakMiddleware({
+  Logger? logger,
+  http.Client? client,
+  required Uri configURL,
+  required String kid,
+}) =>
+    (innerHandler) {
+      return (request) async {
+        var response = await client!.get(
+          configURL,
+        );
+
+        logger?.d('Response config: ${response.body.toString()}');
+
+        final certs = jsonDecode(response.body);
+
+        final cert = (certs['keys'] as List)
+            .firstWhere((element) => (element as Map)['kid'] == kid);
+
+        final rawToken = request.headers['Authorization'];
+        logger?.d('Authorization: $rawToken');
+
+        if (rawToken == null) {
+          return Response(401);
+        }
+
+        if (!rawToken.startsWith('Bearer ')) {
+          return Response(401);
+        }
+
+        final token = rawToken.replaceFirst('Bearer', '').trim();
+
+        if (token.isEmpty) {
+          return Response(401);
+        }
+        // create a JsonWebSignature from the encoded string
+        var jws = JsonWebSignature.fromCompactSerialization(token);
+
+        // extract the payload
+        var payload = jws.unverifiedPayload;
+
+        logger?.d("content of jws: ${payload.stringContent}");
+        logger?.d("protected parameters: ${payload.protectedHeader?.toJson()}");
+
+        // create a JsonWebKey for verifying the signature
+        var jwk = JsonWebKey.fromJson(cert);
+        var keyStore = JsonWebKeyStore()..addKey(jwk);
+
+        // verify the signature
+        var verified = await jws.verify(keyStore);
+        logger?.d("signature verified: $verified");
+
+        if (verified == false) {
+          return Response(401);
+        }
+
         return Future.sync(() => innerHandler(request)).then((response) {
           return response;
-        }, onError: (Object error, StackTrace stackTrace) {
-          if (error is HijackException) throw error;
-
-          throw error;
         });
       };
     };
